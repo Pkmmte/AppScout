9b95735a5a79c939cd820f6dbed547f20b39a2e2
Pkmmte <pkx7225@gmail.com>
2015-06-18 00:31:49 -0700

    Basic manager class setup
    Just my usual setup for such manager classes.

--- appscout/src/main/java/com/pkmmte/appscout/AppScout.java
+++ appscout/src/main/java/com/pkmmte/appscout/AppScout.java
@@ -1,5 +1,31 @@
 package com.pkmmte.appscout;
 
-public class AppScout {
+import android.content.Context;
+import android.support.annotation.NonNull;
 
+public class AppScout {
+	// For logging purposes
+	private static final String TAG = AppScout.class.getSimpleName();
+
+	// Main singleton instance
+	private static volatile AppScout singleton = null;
+
+	// Application context... because of reasons
+	private final Context context;
+
+	/**
+	 * The global default {@link AppScout} instance.
+	 * Call this whenever you want to use this library.
+	 */
+	public static AppScout with(@NonNull Context context) {
+		if(singleton == null)
+			singleton = new AppScout(context.getApplicationContext());
+
+		return singleton;
+	}
+
+	/* Hidden constructor */
+	private AppScout(Context context) {
+		this.context = context;
+	}
 }
