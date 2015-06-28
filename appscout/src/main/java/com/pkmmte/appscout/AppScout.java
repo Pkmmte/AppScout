78a8a870a98f81e98eb499265b99be3323cb5952
Pkmmte <pkx7225@gmail.com>
2015-06-27 19:05:10 -0700

    Load installed apps
    Now loads installed applications. So far so good. ^^

--- appscout/src/main/java/com/pkmmte/appscout/AppScout.java
+++ appscout/src/main/java/com/pkmmte/appscout/AppScout.java
@@ -1,22 +1,32 @@
 package com.pkmmte.appscout;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 
 import com.pkmmte.logger.Logger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class AppScout {
 	// For logging purposes
 	private static final String TAG = AppScout.class.getSimpleName();
 
 	// Special logger
-	protected static final Logger logger = new Logger(TAG, Logger.Level.NONE);
+	protected static final Logger logger = new Logger(TAG, Logger.Level.LOW);
 
 	// Main singleton instance
 	private static volatile AppScout singleton = null;
 
 	// Application context... because of reasons
 	private final Context context;
+
+	// Cache for loaded apps
+	private volatile List<App> installedApps = new ArrayList<>();
 
 	/**
 	 * The global default {@link AppScout} instance.
@@ -54,12 +64,52 @@ public class AppScout {
 		// TODO
 	}
 
+	// TODO
 	protected void load(Request request) {
 		logger.medium("load(" + request + ')');
 		//final CallbackHandler handler = request.handler != null ? request.handler : this.handler;
 		//final boolean safe = request.safe != null ? request.safe : this.safe;
 
-		// TODO
+		// TODO: Notify callback
+
+		// Get list of all installed applications and sort them by name
+		final PackageManager packageManager = context.getPackageManager();
+		final List<ApplicationInfo> packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+		Collections.sort(packages, new ApplicationInfo.DisplayNameComparator(packageManager));
+
+		// Create new local list for installed apps
+		final int numPackages = packages.size();
+		final List<App> installed = new ArrayList<>(numPackages);
+		logger.medium("Found " + numPackages + " installed packages");
+
+		for (int index = 0; index < numPackages; index++) {
+			// Get basic application info
+			final ApplicationInfo packageInfo = packages.get(index);
+			final Intent launchIntent = packageManager.getLaunchIntentForPackage(packageInfo.packageName);
+
+			// Skip invalid intents
+			if (launchIntent == null)
+				continue;
+
+			// TODO: Notify callback
+
+			// Build custom immutable App object
+			App app = new App.Builder()
+					.name(packageInfo.loadLabel(packageManager))
+					.component(Utils.getComponentString(launchIntent))
+					.icon(packageInfo.loadIcon(packageManager))
+					.build();
+
+			// Append to list
+			installed.add(app);
+			logger.low(app.toString());
+		}
+
+		// Replace global installed list with local list
+		installedApps = installed;
+		logger.medium("Loaded " + installed.size() + " installed apps");
+
+		// TODO: Notify callback
 	}
 
 	protected void send(Request request) {
